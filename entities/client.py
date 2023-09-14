79b341096789d5d3fe7ddf9dcc52c9696ddedd0e
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-09-14 20:17:52 +0800

    updates

--- entities/client.py
+++ entities/client.py
@@ -1,49 +1,54 @@
-import socket 
+import socket
 from main import main
 import argparse
+
+
 
 def receive(args):
     while True:
         text = s.recv(1024)
-        print(text.decdode())
-        send(map = text.decode(),args = args)
+        print(text.decode())
+        send(map=text.decode(), args=args)
 
-def send (map, args):
+def send(map, args):
     #map = 'ROB:25,25;OBS1:55,75,270;OBS2:125,95,0;OBS3:155,45,90;OBS4:155,155,270;OBS5:55,135,180'
-    text = main(map, args))#'ROB:15,15;OBS1:215,35,90;OBS2:135,135,90;OBS3:15,95,270;OBS4:55,145,180')#map)#"ROB:15,15;OBS1:45,125,0;OBS2:135,155,270;OBS3:105,105,180;OBS4:145,45,90")
+    text = main(map, args)#'ROB:15,15;OBS1:215,35,90;OBS2:135,135,90;OBS3:15,95,270;OBS4:55,145,180')#map)#"ROB:15,15;OBS1:45,125,0;OBS2:135,155,270;OBS3:105,105,180;OBS4:145,45,90")
     #text = main('ROB:15,15;OBS1:175,15,180;OBS2:85,55,0;OBS3:105,195,270;OBS4:165,165,270;OBS5:35,135,90')
-
     s.send(text.encode())
 
-
 def parse_args():
-    # parse command line arguments
+    """ Parse command line arguments.
+    """
     parser = argparse.ArgumentParser(description="MDP Algorithm Client")
     parser.add_argument(
         "--port", help="Port",
         default=5000, required=True)
+
     parser.add_argument(
-        "--use-inplace", help="Use Inplace Turn", action="store_true",
+        "--use-inplace", help="Use Inplace Place", action="store_true",
         default=False
     )
+
     parser.add_argument(
         "--padding", help="Turning Padding",
         default=8, type=int
     )
 
-
+    return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    HOST = "192.168.27.27"   # The server's hostname or IP address
-    PORT = args.port         #The port used by server
-    if(type(PORT) == str):
+    HOST = '192.168.27.27'  # The server's hostname or IP address
+    PORT = args.port        # The port used by the server
+    if type(PORT) == str:
         PORT = int(PORT)
-
     s = socket.socket()
-    s.connect((HOST,PORT))
+    s.connect((HOST, PORT))
     receive(args)
     # print(args.use_inplace)
     # print(type(args.use_inplace))
     # print(main('ROB:25,25;OBS1:55,75,270;OBS2:125,95,0;OBS3:155,45,90;OBS4:155,155,270;OBS5:55,135,180', args))
 
+
+
+        
--- entities/main.py
+++ entities/main.py
@@ -1,63 +1,127 @@
+
 from utils import Node
-from obstacle
+from obstacle import Obstacle
 from graph import Graph
 from trip_planner import TripPlanner
 import math
 from copy import deepcopy
 
-def main(input_str = "", args = None):
-        # input_str sample ROB:20,20;OBS1:105,105,90;OBS2:155,65,90;OBS3:65,65,270;OBS4:195,105,180;OBS5:130,160,180
-        carNode , obstacles = preprocess(input_str = input_str)
-        graph = Graph(carNode)
-        graph.constructGraph(obstacles)
-        best_path = graph.bruteforce()
-        greedy_path = graph.neartestNeighbour
-        best_path_str = ""
-        greedy_path_str = ""
+def main(input_str = "ROB:20,20;OBS1:105,105,90;OBS2:155,65,90;OBS3:65,65,270;OBS4:195,105,180;OBS5:130,160,180", args=None):
 
-        for item in best_path:
-                best_path_str +=f"{item.key}->"
-        
-        for item in greedy_path:
-                greedy_path_str += f"->{item.key}"
+    # input_str = input()
 
-        
-        print(f"Best path:{best_path_str}")
-        print(f"Greedy Path: {greedy_path_str}")
+    carNode, obstacles = preprocess(input_str= input_str)
 
-        if args is not None:
-                algo = TripPlanner(best_path, obstacles, args.use_inplace, int(args.padding))
-        else :
-                algo = TripPlanner(best_path, obstacles)
-        stm_commands =[]
-        android_commands =[]
-        for item in best_path:
-                item.x=round(item.x)
-                item.y = round(item.y)
-                print((item.x, item.y))
-        
-        current = best_path[0]
-        path = ""
-        corrections = []
-        for next in range (1, len(best_path)):
+    graph = Graph(carNode)
+    graph.constructGraph(obstacles)
+
+    best_path = graph.bruteforce()
+    greedy_path = graph.nearestNeighbour()
+    best_path_str, greedy_path_str = "", ""
+
+    for item in best_path:
+        best_path_str += f"{item.key}->"
+    
+    for item in greedy_path:
+        greedy_path_str += f"->{item.key}"
+
     
 
-def preprocess(input_str):
+    print(f"Best path:{best_path_str}")
+    print(f"Greedy Path: {greedy_path_str}")
+    if args is not None:
+        algo = TripPlanner(best_path, obstacles, args.use_inplace, int(args.padding))
+    else:
+        algo = TripPlanner(best_path, obstacles)
+    stm_commands = []
+    android_commands = []
+    for item in best_path:
+
+        item.x = round(item.x)
+        item.y = round(item.y)
+        print((item.x, item.y))
+
+    current = best_path[0]
+    path = ""
+    corrections = []
+    for next in range (1,len(best_path)):
+
+        dest = deepcopy(best_path[next])
         
-        objects = input_str.split(";")
-        obstacles = []
-        for item in objects:
-                key, pos = item.split(":")
-                if key =="ROB":
-                        
-                    posX, posY = list(map(int,pos.split(",")))
-                    carNode = Node(key, posX, posY, theta=90)
-                
-                else:
-                    posX , posY,pos_image = list(map(int,pos.split(",")))
-                    print(posX, posY, pos_image)
+        print("First Try:")
+        print(((current.x,current.y), (dest.x,dest.y)))
+        trip = algo.planTripAStar(current, dest)
+        if len(trip) == 0:
+            dest = deepcopy(best_path[next])
+            dest.x = dest.x + int(10*math.cos(math.radians(dest.theta)))
+            dest.y = dest.y + int(10*math.sin(math.radians(dest.theta)))
+            print("Second try:")
+            print(((current.x,current.y), (dest.x,dest.y)))
+            trip = algo.planTripAStar(current, dest)
+            if len(trip) == 0:
+                dest = deepcopy(best_path[next])
+                dest.x = dest.x - int((10*math.cos(math.radians(dest.theta))))
+                dest.y = dest.y - int(10*math.sin(math.radians(dest.theta)))
+                print(((current.x,current.y), (dest.x,dest.y)))
+                print ("Third Try:")
+                trip = algo.planTripAStar(current, dest)
+                if len(trip) == 0:
+                    continue
+        path += f"{dest.key}->"
+        stm_instr = algo.generateInstructions(trip, device = "stm")
+        stm_commands.append(';'.join(stm_instr))
+        android_instr = algo.generateInstructions(trip, device = "android")
+        android_commands.append(';'.join(android_instr))
+        f_err, b_err = algo.error_calibrator.getCorrection(dest)
+        corrections.append(f"{f_err},{b_err}")
+        current=dest
 
-                    obstacles.appned(Obstacle(key, posX, posY, pos_image))
+    print(f"Yay! We found {len(stm_commands)} obstacles")
+    stm_commands = "-".join(stm_commands)
+    android_commands = "-".join(android_commands)
+    path = path[:-2]
+    corrections = ";".join(corrections)
 
-        return carNode, obstacles
-                        
+    final_str = stm_commands + "|" + android_commands + "|" + path + "|" + corrections
+
+    return final_str
+
+
+def preprocess(input_str):
+
+    objects = input_str.split(";")
+
+    obstacles = []
+
+    for item in objects:
+
+        key, pos = item.split(":")
+
+        if key == "ROB":
+
+            posX, posY = list(map(int,pos.split(",")))
+            carNode = Node(key, posX, posY, theta= 90)
+        else:
+
+            posX, posY, pos_image = list(map(int,pos.split(",")))
+            print(posX,posY,pos_image)
+            
+            obstacles.append(Obstacle(key, posX, posY, pos_image))
+    return carNode, obstacles 
+
+
+
+
+    
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    # print(main('ROB:25,25;OBS1:55,75,270;OBS2:125,95,0;OBS3:155,45,90;OBS4:155,155,270;OBS5:55,135,180'))
+    print(main('ROB:15,15;OBS1:15,115,90;OBS2:105,45,0;OBS3:195,15,90;OBS4:135,105,270;OBS5:105,115,180;OBS6:155,145,270;OBS7:75,175,180'))
+
+    #print(main("ROB:15,15;OBS1:125,5,0;OBS2:135,155,270;OBS3:85,5,180;OBS4:145,45,90"))
