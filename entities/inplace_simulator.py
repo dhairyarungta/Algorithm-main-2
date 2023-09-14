55172228b5502129c73bcf3fc7489d483c633bba
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-09-15 01:30:20 +0800

    update

--- entities/inplace_simulator.py
+++ entities/inplace_simulator.py
@@ -1,19 +1,21 @@
 from utils import Node
 from constants import TR
-from collision import CollisionDetector 
+from collision import CollisionDetector
 
-bb = TR["Inplace turn"] 
+bb = TR["Inplace turn"]
+
 class Inplace_Simulator:
-    def __init__(self,collison_detector:CollisionDetector):
+
+    def __init__(self,collision_detector:CollisionDetector):
+
         self.height = bb["height"]
         self.width = bb["width"]
         self.st = bb["st"]
         self.side =  bb["side"]
-        self.collision_detector =  collison_detector
-    
-    def returnMeta(self, currentNode:Node, turn = "right"):
-        x, y, theta = currentNode.x, currentNode.y, currentNode.theta
+        self.collision_detector =  collision_detector 
 
+    def returnMeta(self,currentNode:Node, turn = "right"):
+        x, y, theta = currentNode.x, currentNode.y, currentNode.theta
         bbcenter = {
             90:{
                 "left": lambda x,y: (x+self.side-(self.width//2),y-self.st+(self.height//2)),
